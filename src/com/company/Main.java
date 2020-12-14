aa8471cbb31871864a0d71edfad4598f99c622b0
Ancau Adrian <adiancau@yahoo.com>
2020-12-15 01:55:15 +0200

    Read pif from file

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,30 @@
 package com.company;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
 
 public class Main {
+
+    public static List<String> readPif(String filename) {
+        try {
+            File pifFile = new File(filename);
+            Scanner reader = new Scanner(pifFile);
+
+            List<String> ret = new LinkedList<>();
+
+            while (reader.hasNextLine()) {
+                ret.add(reader.nextLine().strip().split(" ")[0]);
+            }
+            reader.close();
+
+            return ret;
+        } catch (FileNotFoundException e) {
+            System.out.println("Could not open file: " + filename);
+            e.printStackTrace();
+            return null;
+        }
+    }
 
     public static void main(String[] args) {
         Grammar grammar = new Grammar();
@@ -13,11 +33,8 @@ public class Main {
         List<Set<Item>> states=Parser.canonicalCollection(grammar);
         LRtable table=new LRtable(states,grammar);
         table.printTable();
-        List<String> w=new ArrayList<>();
-        w.add("a");
-        w.add("b");
-        w.add("b");
-        w.add("c");
+        List<String> w = readPif("PIF.out");
+        System.out.println(w);
         System.out.println(Parser.parse(grammar,w,table));
     }
 }
